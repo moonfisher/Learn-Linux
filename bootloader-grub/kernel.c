de9213d2b100e3e2301ba0f8429a073a56bb082d
Cyril <athenasigh@gmail.com>
2019-03-17 23:56:22 +0800

    modify

--- bootloader-grub/kernel.c
+++ bootloader-grub/kernel.c
@@ -9,20 +9,31 @@ void show_memory_map()
 	uint32_t mmap_addr = glb_mboot_ptr->mmap_addr;
 	uint32_t mmap_length = glb_mboot_ptr->mmap_length;
 
+	print_str("mmap_addr:");
+        print_hex((uint32_t) mmap_addr);
+        print_str("\r\n");
+        print_str("mmap_length:");
+        print_hex((uint32_t) mmap_length);
+        print_str("\r\n");
+
 	mmap_entry_t *mmap = (mmap_entry_t *) mmap_addr;
 	for (mmap = (mmap_entry_t *) mmap_addr; (uint32_t) mmap < mmap_addr + mmap_length; mmap++)
 	{
+    		print_str("base_addr_low:");
 		print_hex((uint32_t) mmap->base_addr_low);
-		print_char('\n');
+		print_str("\n");
+                print_str("base_addr_low_len:");
+                print_hex((uint32_t) mmap->length_low);
+                print_str("\n");
 	}
 }
 
 int kernel_main()
 {
 	print_clear();
-	print_str("\r\n");
+	print_str("kernel_main:");
 	print_hex((uint32_t)(int *)&kernel_main);
-	print_str("\r\n");
+	print_str("\r\nglb_mboot_ptr:");
         print_hex((uint32_t)glb_mboot_ptr);
 	print_str("\r\n");
 	print_str("memory map is below\r\n");
