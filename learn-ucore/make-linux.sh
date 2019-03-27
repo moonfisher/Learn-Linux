f1dc133e32d0b8124bc8694d4f977651064dbd02
Cyril <athenasigh@gmail.com>
2019-03-27 09:21:56 +0800

    modify

---
+++ learn-ucore/make-linux.sh
@@ -0,0 +1,18 @@
+
+rm -rf obj1;
+mkdir obj1;
+
+gcc -Iboot/ -fno-builtin -fno-PIC -Wall -ggdb -m32 -gstabs -nostdinc  -fno-stack-protector -Ilibs/ -Os -nostdinc -c boot/bootasm.S -o obj1/bootasm.o
+gcc -Iboot/ -fno-builtin -fno-PIC -Wall -ggdb -m32 -gstabs -nostdinc  -fno-stack-protector -Ilibs/ -Os -nostdinc -c boot/bootmain.c -o obj1/bootmain.o
+ld -m elf_i386 -nostdlib -N -T tools/boot.ld obj1/bootasm.o obj1/bootmain.o -o obj1/bootblock.elf
+objdump -d obj1/bootblock.elf > obj1/bootblock.asm;
+objcopy -S -O binary obj1/bootblock.elf obj1/bootblock.out;
+
+gcc -o2 -o tools/sign tools/sign.c;
+tools/sign obj1/bootblock.out obj1/bootblock;
+
+rm -rf disk.img;
+dd if=/dev/zero of=disk.img count=10000
+dd if=obj1/bootblock of=disk.img conv=notrunc
+dd if=bin/kernel of=disk.img seek=1 conv=notrunc
+
--- learn-ucore/make-mac.sh
+++ learn-ucore/make-mac.sh
@@ -11,8 +11,8 @@ i386-elf-objcopy -S -O binary obj1/bootblock.elf obj1/bootblock.out;
 gcc -o2 -o tools/sign tools/sign.c;
 tools/sign obj1/bootblock.out obj1/bootblock;
 
-rm -rf floppy.img;
-dd if=obj1/bootblock of=floppy.img bs=512 count=1 conv=notrunc;
-dd if=bin/kernel of=floppy.img skip=1 seek=1 bs=512 count=1;
-
+rm -rf disk.img;
+dd if=/dev/zero of=disk.img count=10000
+dd if=obj1/bootblock of=disk.img conv=notrunc
+dd if=bin/kernel of=disk.img seek=1 conv=notrunc
 
